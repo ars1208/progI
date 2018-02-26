c1b3e8bb5b7e4bc41739debac3d260d310ba9111
ars1208 <36816903+ars1208@users.noreply.github.com>
2018-02-26 22:48:16 +0900

    Add files via upload

--- report_ex1.py
+++ report_ex1.py
@@ -7,10 +7,13 @@ while i < 100:
     if new_place[0] == ".":
         break
     if new_place[0] in place.keys():
+        print("その名前のデータはすでに存在します。上書きしますか？[y/n]")
         choice = input()
         if choice == "y":
             place[new_place[0]] = new_place[1]
+            print("上書きしました。")
         elif choice == "n":
+            print("上書きしませんでした。")
             continue
     place[new_place[0]] = new_place[1]
     i += 1
